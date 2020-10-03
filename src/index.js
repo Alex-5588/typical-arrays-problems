77847104636c448675a7d0472ff6adf6f85aab21
Aleksei Drebezov <57900027+Alex-5588@users.noreply.github.com>
2020-10-04 00:36:54 +0300

    fix: bug
    fix: bug

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ exports.max = function max (array) {
 
 exports.avg = function avg (array) {
   if(Array.isArray(array) && array.lengh === 0) {  
-  var sumArrs;
+  let sumArrs;
   for(let i = 0; i < array.lengh; i++) {
     sumArrs += array[i];
   }
