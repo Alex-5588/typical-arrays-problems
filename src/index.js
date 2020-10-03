fd072c0f9ec258f18666aabe57f086a3f1fe58d7
Aleksei Drebezov <57900027+Alex-5588@users.noreply.github.com>
2020-10-04 01:10:53 +0300

    fix: rewrite functions
    fix: rewrite functions

--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
 exports.min = function min (array) {
-  var len = array.length, min = Infinity;
-  while (len--) {
-    if (array[len] < min) {
-      min = array[len];
-    } else {
-    return 0;
-    }
+  if(Array.isArray(array) && array.lengh !== 0) {
+    return Math.min.apply(null, array);
+  } else {
+      return 0;
   }
-  return Math.min.apply(null, array);
 };
 
 exports.max = function max (array) {
-  var len = array.length, max = -Infinity;
-  while (len--) {
-    if (array[len] > max) {
-      max = array[len];
-    } else {
+  if(Array.isArray(array) && array.lengh !== 0) {
+    return Math.min.apply(null, array);
+  } else {
     return 0;
-    }
   }
-  return Math.max.apply(null, array);
 };
 
 exports.avg = function avg (array) {
@@ -29,9 +21,8 @@ exports.avg = function avg (array) {
     sumArrs += array[i];
   }
   let mean = sumArrs / array.lengh;
-      return mean;                             
+      return mean;
   } else {
     return 0;
   }
 };
-
